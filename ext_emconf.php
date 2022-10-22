655d4c5cb4ae491be7c173627667393554e31f48
Raphael Thanner <r.thanner@zeroseven.de>
2022-10-22 21:16:48 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Local fonts',
+    'description' => 'Load google fonts locally',
+    'category' => 'plugin',
+    'author' => 'Raphael Thanner',
+    'author_email' => 'r.thanner@zeroseven.de',
+    'author_company' => 'zeroseven design studios GmbH',
+    'state' => 'beta',
+    'clearCacheOnLoad' => 1,
+    'version' => '0.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-11.5.99'
+        ],
+        'suggests' => [
+        ]
+    ]
+];
