f3f1ca38f1f3cf8510436c6d6cded5bc6236ee2c
Raphael Thanner <r.thanner@zeroseven.de>
2022-10-27 13:00:53 +0200

    It's finally here! The fully automated solution to the Google font problem.

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,14 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Font loader',
-    'description' => 'Load Google Fonts and Fontawesome to share from your TYPO3',
+    'description' => 'With this extension external fonts can be loaded directly into your TYPO3 ecosystem and will be delivered from your local webserver',
     'category' => 'plugin',
     'author' => 'Raphael Thanner',
     'author_email' => 'r.thanner@zeroseven.de',
     'author_company' => 'zeroseven design studios GmbH',
-    'state' => 'beta',
+    'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '0.1.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99'
